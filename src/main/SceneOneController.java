9b2beab45688e58497abb7848dc6ecbdf0092a60
Aziz Sonawalla <sonawallaa@gmail.com>
2019-04-14 21:39:01 -0700

    Add validation to directory chooser

--- src/main/SceneOneController.java
+++ src/main/SceneOneController.java
@@ -6,13 +6,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import org.apache.commons.io.FilenameUtils;
 
 public class SceneOneController implements Initializable {
 
@@ -21,6 +24,7 @@ public class SceneOneController implements Initializable {
 
     private String inputDirectory = "No input directory selected";
     private String outputDirectory = "No output directory selected";
+    private ArrayList<String> inputImagePaths = new ArrayList<>();
 
     // UI Elements
     @FXML private Label inputDirectoryLabel;
@@ -29,6 +33,10 @@ public class SceneOneController implements Initializable {
     @FXML private Button outputDirectoryChooser;
     @FXML private CheckBox autoChoosePreviewImage;
     @FXML private Button previewImageChooser;
+    @FXML private Label inputDirectoryValidationStatus;
+
+    // App Config
+    private final ArrayList<String> supportedFileTypes = new ArrayList<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -37,6 +45,8 @@ public class SceneOneController implements Initializable {
         autoChoosePreviewImage.setAllowIndeterminate(false);
         autoChoosePreviewImage.setSelected(true);
         previewImageChooser.setDisable(true);
+
+        supportedFileTypes.add("jpeg");
     }
 
     @FXML
@@ -46,7 +56,18 @@ public class SceneOneController implements Initializable {
         if(selectedDirectory != null){
             if (event.getSource().equals(inputDirectoryChooser)) {
                 this.inputDirectory = selectedDirectory.getAbsolutePath();
-                inputDirectoryLabel.setText(this.inputDirectory); // check for at least 1 image
+                inputDirectoryLabel.setText(this.inputDirectory);
+                inputDirectoryValidationStatus.setTextFill(new Color(1.0,1.0,1.0,0.5));
+                inputDirectoryValidationStatus.setText("Checking...");
+                String validationResult = inputDirectoryValidator(this.inputDirectory);
+                if (validationResult != "") {
+                    inputDirectoryValidationStatus.setTextFill(new Color(1.0,0,0,1.0));
+                    inputDirectoryValidationStatus.setText(validationResult);
+                } else {
+                    inputDirectoryValidationStatus.setTextFill(new Color(0.0,1.0,0,0.8));
+                    int numberOfDiscoveredImages = inputImagePaths.size();
+                    inputDirectoryValidationStatus.setText(String.format("Found %d images", numberOfDiscoveredImages));
+                }
             }
             if (event.getSource().equals(outputDirectoryChooser)) {
                 this.outputDirectory = selectedDirectory.getAbsolutePath();
@@ -76,5 +97,29 @@ public class SceneOneController implements Initializable {
             System.out.print("Will manually choose preview image\r");
             previewImageChooser.setDisable(false);
         }
+    }
+
+    private String inputDirectoryValidator(String inputDirectoryPath) {
+        File folder = new File(inputDirectoryPath);
+        File[] listOfFiles = folder.listFiles();
+        int numOfFiles = listOfFiles.length;
+
+        if (numOfFiles == 0) {
+            return "Selected source directory does not contain any files";
+        }
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            String imagePath = listOfFiles[i].getAbsolutePath();
+            String fileExtention = FilenameUtils.getExtension(imagePath);
+            if (listOfFiles[i].isFile() && supportedFileTypes.contains(fileExtention)) {
+                inputImagePaths.add(imagePath);
+            }
+        }
+
+        if (inputImagePaths.size() == 0) {
+            return "Could not find any files with supported formats in source directory";
+        }
+
+        return "";
     }
 }
